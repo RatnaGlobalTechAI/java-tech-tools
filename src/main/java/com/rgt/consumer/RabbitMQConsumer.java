ff21f3b590b3157b048fe6d16225e082fe742e99
sandeshtirlotkar <sandesh.tirlotkar@ratnaglobaltech.com>
2023-11-03 10:21:14 +0530

    RabbitMQ Consumer

---
+++ src/main/java/com/rgt/consumer/RabbitMQConsumer.java
@@ -0,0 +1,24 @@
+package com.rgt.consumer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.stereotype.Service;
+
+import com.rgt.request.UserRequest;
+
+@Service
+public class RabbitMQConsumer {
+
+    private  static  final Logger LOGGER = LoggerFactory.getLogger(RabbitMQConsumer.class);
+
+    @RabbitListener(queues = {"${rabbitmq.queue.name}"})
+    public void consume(String message){
+        LOGGER.info(String.format("Received message -> %s", message));
+    }
+    
+    @RabbitListener(queues = {"${rabbitmq.queue.json.name}"})
+    public void consume(UserRequest user){
+        LOGGER.info(String.format("Received message -> %s", user));
+    }
+}
---
+++ src/main/java/com/rgt/request/UserRequest.java
@@ -0,0 +1,15 @@
+package com.rgt.request;
+
+import lombok.Data;
+
+@Data
+public class UserRequest {
+
+	
+	private Long empId;
+	private String username;
+	private String email;
+	
+	
+	
+}
